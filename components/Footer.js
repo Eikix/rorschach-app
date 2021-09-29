6ffabc7507ebf8204ec22c0b6503ed4321d4c307
Eikix <66871571+Eikix@users.noreply.github.com>
2021-09-29 21:10:20 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
                         className="hover:text-color6"
                         href="https://www.linkedin.com/in/florence-damien-a47ba116b/"
                         target="_blank"
-                        rel="noopener noreferrer"
+                        rel="noreferrer noopener"
                     >
                         Florence Damien
                     </a>
@@ -20,7 +20,7 @@ const Footer = () => {
                         className="hover:text-color6"
                         href="https://www.linkedin.com/in/elias-tazartes-a13450155/"
                         target="_blank"
-                        rel="noopener noreferrer"
+                        rel="noreferrer noopener"
                     >
                         Elias Tazartes
                     </a>
